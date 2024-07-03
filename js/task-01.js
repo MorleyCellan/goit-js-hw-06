2cb11eaae65f23cb81bbaea24dc955dbe3e2f78d
Morley Cellan <morley@morce.dev>
2024-07-03 23:35:57 +0800

    Update js/task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -3,8 +3,9 @@ console.log(
   "Number of categories: " + categories.querySelectorAll("h2").length
 );
 
-const categoryList = categories.querySelectorAll(".item");
-for (let category of categoryList) {
-  console.log("Category: " + category.querySelector("h2").textContent);
-  console.log("Elements: " + category.querySelectorAll("ul li").length);
+const list = categories.querySelectorAll(".item");
+for (let item of list) {
+  console.log("\n");
+  console.log("Category: " + item.querySelector("h2").textContent);
+  console.log("Elements: " + item.querySelectorAll("ul li").length);
 }
