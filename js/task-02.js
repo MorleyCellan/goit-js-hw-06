07327cf4da85fd7b91c3a4aa715bd317415b0005
Morley Cellan <morley@morce.dev>
2024-07-04 04:47:47 +0800

    Add js/task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+let container = document.getElementById("ingredients");
+
+for (let ingredient of ingredients) {
+  let li = document.createElement("li");
+  li.innerText = ingredient;
+  container.appendChild(li);
+  console.log((li.innerText = ingredient));
+}
