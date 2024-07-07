b56b294a58603bc14dfb154b23db61376115d14d
Morley Cellan <morley@morce.dev>
2024-07-07 16:41:52 +0800

    Remove 2 lines of code

--- js/task-05.js
+++ js/task-05.js
@@ -1,7 +1,5 @@
 const input = document.getElementById("name-input");
 const output = document.getElementById("name-output");
-const inputq = document.querySelector("#name-input");
-const outputq = document.querySelector("#name-output");
 
 const getInput = (event) => {
   // const { value, type, placeholder, id } = event.currentTarget;
