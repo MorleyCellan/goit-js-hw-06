ed8f4f7b50baf18bfa696adc2d4ec09758829b62
Morley Cellan <morley@morce.dev>
2024-07-09 10:27:44 +0800

    Add js/task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,10 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const btn = document.querySelector(".change-color");
+const handleClick = () => {
+  document.querySelector("span.color").textContent = getRandomHexColor();
+  document.querySelector("body").style.backgroundColor = getRandomHexColor();
+};
+
+btn.addEventListener("click", handleClick);
