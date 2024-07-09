3e6c658d59d03feced55859be272fb94e15050e9
Morley Cellan <morley@morce.dev>
2024-07-09 09:55:24 +0800

    Add js/task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const loginForm = document.querySelector(".login-form");
+const handleSubmit = (event) => {
+  event.preventDefault();
 
+  const {
+    elements: { email, password },
+  } = event.target;
+  const isEmpty = email.value.length === 0 || password.value.length === 0;
+
+  if (isEmpty) alert("All fields must be filled in.");
+  if (!isEmpty) {
+    const formData = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(formData);
+  }
+
+  return event.target.reset();
+};
+
+loginForm.addEventListener("submit", handleSubmit);
